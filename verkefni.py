e1f170260301405e09919af10c06c9276bb8d866
BendiktSexyMan <benediktsi228@nemi.tskoli.is>
2017-01-28 23:12:48 +0000

    kláraði dæmi 3

--- verkefni.py
+++ verkefni.py
@@ -9,3 +9,19 @@ while val != "4":
         fornafn = input("Hvert er nafn þitt?\n")
         eftirnafn = input("Hæ " + fornafn + "!\nHvert er eftirnafn þitt?\n")
         print("Hæ", fornafn + " " + eftirnafn)
+    if val == "3":
+        text = input("Gefðu mér texta!\n")
+        ha = 0
+        la = 0
+        eftir = 0
+        for x in range(len(text)):
+            if text[x].isupper():
+                ha += 1
+            elif text[x].islower():
+                la += 1
+                if x > 0:
+                    if text[x-1].isupper():
+                        eftir += 1
+        print("Það voru", ha, "hástafir\nÞað voru", la, "lágstafir\nÞað voru", eftir, "lágstafir sem fylgdu hástöfum!")
+    val = input("Hvað viltu gera?\n1: 2 tölur?\n2: Nöfn?\n3: Hástafir og Lágstafir?\n4: Hætta?\n")
+print("Takk Fyrir Mig!")
