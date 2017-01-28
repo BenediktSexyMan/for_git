104b1f174ca57f36c352cdf2eb9c351dea836097
BendiktSexyMan <benediktsi228@nemi.tskoli.is>
2017-01-28 23:11:38 +0000

    kláraði dæmi 2

--- verkefni.py
+++ verkefni.py
@@ -5,3 +5,7 @@ while val != "4":
         num1 = int(input("Gefðu mér tvær heiltölur!\n"))
         num2 = int(input())
         print("Tölurnar lagðar saman eru:", num1 + num2, "\nTölurnar margfaldaðar saman eru:", num1 * num2)
+    if val == "2":
+        fornafn = input("Hvert er nafn þitt?\n")
+        eftirnafn = input("Hæ " + fornafn + "!\nHvert er eftirnafn þitt?\n")
+        print("Hæ", fornafn + " " + eftirnafn)
